df4fa45c7384d48efb3be08282170c2fc23654f0
Ananta Durga Prasad <ananta.durgaprasad@wizcart.com>
2024-11-26 06:20:04 +0530

    add vpc module

---
+++ VPC/locals.tf
@@ -0,0 +1,18 @@
+
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+locals {
+  public_subnets = { # this is for subnet creation across azs equally
+    for idx, cidr_block in var.public_subnets : cidr_block => {
+      cidr_block        = cidr_block
+      availability_zone = data.aws_availability_zones.azs.names[idx % length(data.aws_availability_zones.azs.names)]
+    }
+  }
+  private_subnets = {
+    for idx, cidr_block in var.private_subnets : cidr_block => {
+      cidr_block        = cidr_block
+      availability_zone = data.aws_availability_zones.azs.names[idx % length(data.aws_availability_zones.azs.names)]
+    }
+  }
+}
