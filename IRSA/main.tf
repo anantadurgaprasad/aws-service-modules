50ffa743c6a3e790320054a2e46b09dbb8dabac1
Ananta Durga Prasad <anantadurgaprasadar@gmail.com>
2024-12-03 21:30:34 +0530

    add IRSA

---
+++ IRSA/main.tf
@@ -0,0 +1,38 @@
+data "aws_caller_identity" "current" {}
+locals {
+  tags = {}
+}
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${var.oidc_provider}"]
+    }
+    condition {
+      test = "StringEquals"
+      variable = "${var.oidc_provider}:aud"
+      values = ["sts.amazonaws.com"]
+    }
+    condition {
+      test = "StringEquals"
+      variable = "${var.oidc_provider}:sub"
+      values = ["system:serviceaccount:${var.namespace}:${var.service_account}"]
+    }
+  }
+}
+
+
+
+resource "aws_iam_role" "role" {
+  name = var.role_name
+  tags = merge(local.tags, var.tags)
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  for_each = set(var.policy_arns)
+  role       = aws_iam_role.role.name
+  policy_arn = each.key
+}
---
+++ IRSA/variables.tf
@@ -0,0 +1,26 @@
+variable "oidc_provider" {
+  description = "OIDC provider value"
+  type = string
+}
+variable "namespace" {
+  description = "Namespace of service account associated with IRSA"
+  type = string
+}
+variable "service_account" {
+    description = "Service Account associated with IRSA"
+    type = string
+  
+}
+variable "role_name" {
+  description = "Role Name of the IAM Role"
+  type = string
+}
+variable "policy_arns" {
+  description = "List of all the Policy arns to attach"
+  type = list(string)
+}
+variable "tags" {
+  description = "Tags for the resources"
+  type = map(string)
+  default = {}
+}
